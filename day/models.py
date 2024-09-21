f0d798dc30b8506a10e9138eb2db0788abddd164
bishal89 <bishalmurmu150@gmail.com>
2024-09-21 21:43:01 +0545

    updated the todo index part

--- day/models.py
+++ day/models.py
@@ -5,8 +5,6 @@ from django.db import models
 class Todo(models.Model):
     is_completed=models.BooleanField(default=False)
     text=models.CharField(max_length=100)
-    
-    
     updated_at=models.DateTimeField(auto_now=True)
     created_at=models.DateTimeField(auto_now_add=True)
     
--- day/views.py
+++ day/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render,HttpResponse
-
+from django.shortcuts import render,HttpResponse,redirect
+from . models import Todo
 # Create your views here.
 
 def home(request):
-    return render(request,'index.html')
+    if request.method == 'POST':
+        data=request.POST['kushalta']
+        Todo.objects.create(
+            is_completed=True,
+            text=data
+            
+        )
+        return redirect('home')
+        
+    else:
+        data=Todo.objects.all()
+        context={
+            'datas':data
+        }
+        return render(request,'index.html',context)
     

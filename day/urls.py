168d84c429e632da6a9e8f0811ad07507e67b0f4
bishal89 <bishalmurmu150@gmail.com>
2024-09-22 22:07:37 +0545

    markdown up

--- day/urls.py
+++ day/urls.py
@@ -4,7 +4,13 @@ from . import views
 
 urlpatterns = [
     
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    
+    path('delete/<int:id>/',views.delete_item,name='delete_item'),
+    
+    path('completed/<int:id>/',views.completed,name='com'),
+    
+    path('uncompleted/<int:id>/',views.uncompleted,name='uncom'),
    
 ]
 
--- day/views.py
+++ day/views.py
@@ -6,16 +6,40 @@ def home(request):
     if request.method == 'POST':
         data=request.POST['kushalta']
         Todo.objects.create(
-            is_completed=True,
+            # is_completed=True,
             text=data
             
         )
         return redirect('home')
         
     else:
-        data=Todo.objects.all()
+        data=Todo.objects.filter(is_completed=False)
+        iscompleted=Todo.objects.filter(is_completed=True)
         context={
-            'datas':data
+            'datas':data,
+            'iscompleted':iscompleted
         }
         return render(request,'index.html',context)
+    
+    
+ 
+def delete_item(request,id):
+    data = Todo.objects.get(id=id)
+    data.delete()
+    return redirect('home')
+    
+    
+    
+def completed(request,id):
+    data=Todo.objects.get(id=id)
+    data.is_completed=True
+    data.save()
+    return redirect('home')    
+
+
+def uncompleted(request,id):
+    data=Todo.objects.get(id=id)
+    data.is_completed=False
+    data.save()
+    return redirect('home')
     
